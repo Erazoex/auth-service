be1c3f8f21033b63ddd4aa635fdf10f1f48cfeb1
erazoex <3012750170101@ingenieria.usac.edu.gt>
2022-12-09 16:48:20 -0600

    adding some changes to login.js for using correctly a private key

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 const fs = require('fs')
-const secret = fs.readFileSync('./rsa_key_pair/private.pem')
+const secret = fs.readFileSync('./rsa_key_pair/newPrivate.pem')
 
 router.post('/login', (req, res) => {
     const user = req.body
     // TODO: here should go the database query search for the user that is making the request 
-    jwt.sign({user}, secret, { algorithm: 'RS256' }, (err, token) => {
+    jwt.sign({user}, secret.toString(), { algorithm: 'RS256' }, async(err, token) => {
         if (err) {
             console.log(err)
             res.sendStatus(500)
